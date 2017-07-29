217a6ae2f67805b448f73715eff39c560440ea14
kikukawa_ryoji <ryoji19950109@gmail.com>
2017-07-29 14:01:09 +0900

    Hello EzPickできました。

---
+++ src/main/java/ezpick/EzPickApplication.java
@@ -0,0 +1,11 @@
+package ezpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzPickApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EzPickApplication.class, args);
+    }    
+}
---
+++ src/main/java/ezpick/EzPickRestControler.java
@@ -0,0 +1,14 @@
+package ezpick;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ezpick")
+public class EzPickRestControler {
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
